31140549fe7f3c2804adbf5566a723fc302aea15
Lei Zhi Guang <rayguang123@gmail.com>
2023-02-08 06:50:01 +0800

    update server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    // const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://suddenly-gpt.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -107,7 +108,7 @@ const handleSubmit = async (e) => {
     } else {
         const err = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong"
+        messageDiv.innerHTML = "Something went wrong..."
         alert(err)
     }
 }
